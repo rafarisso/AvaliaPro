61c4bef115dc9035ba3027a1bad2aaf90b43f9b2
Rafael Risso <risso_rafa@hotmail.com>
2025-11-22 16:03:37 -0300

    Update env.js

--- public/env.js
+++ public/env.js
@@ -3,6 +3,4 @@ window.ENV = {
   VITE_SUPABASE_URL: "https://mivwewweekxkawovceqx.supabase.co",
   VITE_SUPABASE_ANON_KEY:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pdndld3dlZWt4a2F3b3ZjZXF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1MzI3MjEsImV4cCI6MjA3NzEwODcyMX0.TmZdaC4ETF9nqjQ8zL-ofO2U_ncaxlI2lYT_dJW9FZA",
-   VITE_GEMINI_API_KEY: "AIzaSyBMakhJ1ReZYVoyL0Qn_ntWZEUMVBD64rA",
-  VITE_GEMINI_MODEL: "gemini-2.0-flash",
 }
